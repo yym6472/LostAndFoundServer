1888081ef696af5929980e7ed8cc3a86d0984c5b
StayGold <yanyuanmeng1996@qq.com>
2018-04-09 00:16:23 +0800

    Added doGet method of download image servlet.

--- src/main/java/com/yymstaygold/lostandfound/server/servlet/DownloadImageServlet.java
+++ src/main/java/com/yymstaygold/lostandfound/server/servlet/DownloadImageServlet.java
@@ -1,14 +1,12 @@
 package com.yymstaygold.lostandfound.server.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by yanyu on 2018/4/8.
@@ -22,18 +20,33 @@ public class DownloadImageServlet extends HttpServlet {
         String imagePath = in.readUTF();
         in.close();
 
-        DataOutputStream out = new DataOutputStream(response.getOutputStream());
-        DataInputStream image = new DataInputStream(new FileInputStream(imagePath));
-        int b;
-        while ((b = image.read()) != -1) {
-            out.write(b);
-        }
-        image.close();
-        out.close();
+        File imageFile = new File(imagePath);
+        outputFileToStream(imageFile, response);
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        String imagePath = (String) request.getParameter("imagePath");
+        System.out.println("IMAGEPATH: " + imagePath);
+        if (imagePath != null) {
+            File imageFile = new File(imagePath);
+            outputFileToStream(imageFile, response);
+        }
+    }
+
+    private void outputFileToStream(File file, HttpServletResponse response)
+            throws IOException {
+        if (file.exists()) {
+            DataOutputStream out = new DataOutputStream(response.getOutputStream());
+            DataInputStream image = new DataInputStream(new FileInputStream(file));
+            int b;
+            while ((b = image.read()) != -1) {
+                out.write(b);
+            }
+            image.close();
+            out.close();
+        }
     }
 }
